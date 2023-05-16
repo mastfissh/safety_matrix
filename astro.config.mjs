638a0135368841ece5c5353f70c42608c342add3
Justin <mastfish@gmail.com>
2023-05-16 15:18:08 +1000

    build content collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mastfissh.github.io',
-  base: '/safety_matrix',
+  // site: 'https://mastfissh.github.io',
+  // base: '/safety_matrix',
+  integrations: [mdx()]
 });
