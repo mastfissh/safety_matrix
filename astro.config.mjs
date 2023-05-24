0ce52fd92503e7921fc5bd898c0042e1d1e68088
Justin <mastfish@gmail.com>
2023-05-24 18:43:38 +1000

    massively improve styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mastfissh.github.io',
   base: '/safety_matrix',
-  integrations: [mdx()]
+  integrations: [mdx(), tailwind()],
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
 });
