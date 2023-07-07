67f179df63b149e4f8b156763e9ba44c7c64af77
Justin <mastfish@gmail.com>
2023-07-07 11:24:51 +1000

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://psychcombo.com',
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), sitemap()]
 });
