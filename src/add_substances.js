dd383a8c3d48ab67626519d807d04e8c5af884f8
Justin <mastfish@gmail.com>
2023-07-12 20:14:02 +1000

    add kava to json

--- src/add_substances.js
+++ src/add_substances.js
@@ -2,12 +2,35 @@ import * as fs from 'fs';
 const input = fs.readFileSync('./src/data.json', 'utf8')
 let data = JSON.parse(input)
 
-// Meditation should be LRS (low risk synergy), high confidence all combos
-//
-let drugs = data["drugs"]
-for (let drug of drugs){
-  data['LRS'].push(["Meditation", drug])
-  data["High confidence"].push(["Meditation", drug])
+let risk_levels = data["risk_levels"]
+for (let risk_level of risk_levels){
+  let slice = data[risk_level]
+  for (let pair of slice){
+    let temp = JSON.stringify(pair)
+    for (let arr of pair){
+      if (arr == 'Banisteriopsis caapi') {
+        temp = temp.replaceAll('Banisteriopsis caapi','Kava')
+        let new_pair = JSON.parse(temp)
+        slice.push(new_pair)
+      }
+    }
+  }
 }
+
+let conf_levels = data["conf_levels"]
+for (let conf_level of conf_levels){
+  let slice = data[conf_level]
+  for (let pair of slice){
+    let temp = JSON.stringify(pair)
+    for (let arr of pair){
+      if (arr == 'Banisteriopsis caapi') {
+        temp = temp.replaceAll('Banisteriopsis caapi','Kava')
+        let new_pair = JSON.parse(temp)
+        slice.push(new_pair)
+      }
+    }
+  }
+}
+
 let out_data = JSON.stringify(data)
 fs.writeFileSync('./src/data.json', out_data);
