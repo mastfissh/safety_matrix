d1f625265404261988e964df581c52983149bdb3
Justin <mastfish@gmail.com>
2023-08-03 23:06:28 +1000

    tweak webworker

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,21 +7,48 @@ import image from "@astrojs/image";
 import serviceWorker from "astrojs-service-worker";
 import preact from "@astrojs/preact";
 
+let transform = function(list){
+  let out = []
+  let allow
+  for (let item of list){
+    allow = true
+    if (item.url.includes('combos')){
+      allow = false
+    }
+    if (item.url.includes('assets')){
+      allow = false
+    }
+    if (allow){
+      out.push(item)
+    }
+  }
+  return {'manifest': out}
+}
+
 let workbox_config = {
   workbox: {
-    inlineWorkboxRuntime: true,
+    additionalManifestEntries: ['/', 'index.html', '/index.html'],
+    manifestTransforms: [transform],
+    // importScripts: ['worker.js'],
+    // runtimeCaching: [
+    //   {
+    //     urlPattern: /combos/,
+    //     handler: 'StaleWhileRevalidate',
+    //     // navigateFallback: '/offline/',
+    //   },
+    // ]
   }
 }
 
 export default defineConfig({
   site: 'https://psychcombo.com/',
   integrations: [
-    // serviceWorker(workbox_config),
+    serviceWorker(workbox_config),
     mdx(),
     tailwind(),
     sitemap(),
     image({ serviceEntryPoint: '@astrojs/image/sharp' }),
     preact(),
-    compress()
+    compress(),
   ]
 });
---
+++ public/worker.js
@@ -0,0 +1,1 @@
+console.debug('HERE',this)
