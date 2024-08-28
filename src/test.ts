6ac4dd2c767401277ecab1c58ad2d8a5d6f384fb
mastfish <mastfish@gmail.com>
2024-08-28 00:08:43 -0400

    test ts

---
+++ src/test.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs';
+import { combo, risk, drug_css_prefix, confidence, linkify } from './util.ts';
+let main = async function(){
+  let output = 0
+  const input = fs.readFileSync('./src/data.json', 'utf8')
+  let data = JSON.parse(input)
+  let drugs = data["drugs"]
+  const pages = fs.readdirSync("./src/content/combos/")
+  const combos = []
+  drugs.map((drugrow) => {
+    drugs.map((drugcell) => {
+      let slug = linkify(combo([drugrow,drugcell]))
+      combos.push(`${slug}.mdx`)
+    })
+  })
+  const errors = []
+  for (let page of pages){
+    if (!combos.includes(page)){
+      errors.push("No combo found for: " + page)
+    }
+  }
+  console.debug(errors)
+  process.exit(errors.length)
+}
+main()
