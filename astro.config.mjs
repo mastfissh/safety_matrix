f8b597ba31eb4bde5eea07b3c5b1f4dc32f9e5ae
Justin <mastfish@gmail.com>
2025-03-14 13:43:01 +1100

    remove service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,31 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import compress from "astro-compress";
 import { defineConfig } from 'astro/config';
-import serviceWorker from "astrojs-service-worker";
-import fs from 'fs';
-import { dirname, path } from 'path';
-import { fileURLToPath } from 'url';
-
-let transform = function (list) {
-  let out = []
-  for (let item of list) {
-    item.url = item.url.replace('/index.html', '/')
-    out.push(item)
-  }
-  out.reverse()
-  let count = { "file_count": out.length }
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
-  fs.writeFileSync(__dirname + '/dist/file_count.json', JSON.stringify(count));
-  return { 'manifest': out }
-}
-
-let workbox_config = {
-  workbox: {
-    additionalManifestEntries: ['/'],
-    manifestTransforms: [transform],
-  }
-}
 
 export default defineConfig({
   site: 'https://psychcombo.com/',
@@ -36,7 +11,6 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   integrations: [
-    serviceWorker(workbox_config),
     mdx(),
     sitemap(),
     preact(),
